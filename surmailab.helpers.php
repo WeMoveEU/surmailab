29974c3129e3f7ddfcda5542dd8403f636789c3b
scardinius <scardinius@chords.pl>
2016-11-09 16:45:55 +0100

    Move dictionary functions to helpers file

--- surmailab.helpers.php
+++ surmailab.helpers.php
@@ -107,6 +107,53 @@ function _surmailab_get_linklabel2($language) {
   }
 }
 
+
+/**
+ * @param $language
+ *
+ * @return string
+ */
+function _surmailab_choose_fromname($language) {
+  switch ($language) {
+    case 'es':
+      return 'Virginia - Movemos Europa';
+    case 'de':
+      return 'Jörg Rohwedder - WeMove.EU';
+    case 'fr':
+      return 'Mika Leandro - WeMove.EU';
+    case 'it':
+      return 'Olga - WeMove.EU';
+    case 'pl':
+      return 'Julia - WeMove.EU';
+    default:
+      return 'Julia - WeMove.EU';
+  }
+}
+
+
+/**
+ * @param $language
+ *
+ * @return int
+ */
+function _surmailab_choose_footer($language) {
+  switch ($language) {
+    case 'es':
+      return 13;
+    case 'de':
+      return 10;
+    case 'fr':
+      return 12;
+    case 'it':
+      return 11;
+    case 'pl':
+      return 15;
+    default:
+      return 14;
+  }
+}
+
+
 function _surmailab_compose_link($surveyId, $language, $label) {
   $url = $_SERVER['HTTP_HOST'].'/'.$language.'/node/'.$surveyId;
   return '<a href="https://'.$url.'">'.$label.'</a>';
